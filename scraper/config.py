9c13a95bb099f1871b7802260c7b6c0444bd4860
Yarden <yardenron4@gmail.com>
2019-02-09 17:55:07 +0200

    Adding to Yahoo Scraper the ability to scrape dividends details (without implementation yet

--- scraper/config.py
+++ scraper/config.py
@@ -1,4 +1,5 @@
 YAHOO_FINANCE = {
     "base_url": "https://finance.yahoo.com/",
-    "analysis": "quote/%s/analysis"
+    "analysis": "quote/%s/analysis",
+    "statistics": "quote/%s/key-statistics"
 }
--- scraper/yahoo_scraper.py
+++ scraper/yahoo_scraper.py
@@ -36,6 +36,46 @@ class YahooScraper:
         egr_string = parser.find_all("td", class_="Ta(end) Py(10px)")[16].get_text()
         return float(egr_string.strip('%'))/100
 
+
+    def get_company_dividends_details(self, ticker):
+        url = self.base_url + YAHOO_FINANCE["statistics"] % ticker
+        self.__add_param_to_url("p", ticker)
+        self.__add_param_to_url(".tsrc", "fin-srch")
+
+        self.logger.debug("Scraping dividends details of company [%s] from Yahoo Finance, url=%s", ticker, url)
+
+        response = session.get(url)
+
+        self.__remove_param_from_url("p")
+        self.__remove_param_from_url(".tsrc")
+
+        if response.status_code != 200:
+            raise ApiError(url, response.status_code)
+
+        payout_ratio = self.__extract_payout_ratio_from_html(response.content)
+        forward_dividend_yield = self.__extract_forward_dividend_yield_from_html(response.content)
+
+        dividends_details = {
+            "Payout Ratio": payout_ratio,
+            "Forward Dividend Yield": forward_dividend_yield
+        }
+
+        self.logger.debug("Successfully scraped dividends details of company [%s] from Yahoo Finance, url=%s",
+                          ticker, url)
+        return dividends_details
+
+    def __extract_payout_ratio_from_html(self, html):
+        parser = BeautifulSoup(html, 'html.parser')
+        # Not so good practice - may be fragile
+        egr_string = parser.find_all("td", class_="Ta(end) Py(10px)")[16].get_text()
+        return float(egr_string.strip('%'))/100
+
+    def __extract_forward_dividend_yield_from_html(self, html):
+        parser = BeautifulSoup(html, 'html.parser')
+        # Not so good practice - may be fragile
+        egr_string = parser.find_all("td", class_="Ta(end) Py(10px)")[16].get_text()
+        return float(egr_string.strip('%'))/100
+
     def __add_param_to_url(self, param, value):
         if param is not None:
             session.params[param] = value
